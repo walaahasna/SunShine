f0b0f2005ef1a0185da489dfa0833dd5227688d5
walaahasna <w_alaa1992@windowslive.com>
2016-01-06 12:58:14 +0300

    Execute AsyncTask to refresh button

--- app/src/main/java/com/example/walaa_p/sunshine/MainActivity.java
+++ app/src/main/java/com/example/walaa_p/sunshine/MainActivity.java
@@ -21,8 +21,10 @@ public class MainActivity extends AppCompatActivity{
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
